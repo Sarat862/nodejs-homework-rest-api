0356a450448c52d6e9b5653cd6a6c06f3f87b4c2
Sarat862 <sarat862@gmail.com>
2022-12-16 14:17:53 +0200

    Done home work 03-mongodb

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,19 +2,21 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 const { ctrlWrapper } = require('../../helpers');
-const { validateBody } = require('../../middlewars');
-const schemas = require('../../schemas/contacts');
+const { validateBody, isValidId } = require('../../middlewars');
+const {schemas} = require('../../models/contact');
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(ctrl.listContacts));
 
-router.get('/:contactId', ctrlWrapper(ctrl.getContactById));
+router.get('/:contactId', isValidId, ctrlWrapper(ctrl.getContactById));
 
 router.post('/', validateBody(schemas.addSchema, "missing required name field"), ctrlWrapper(ctrl.addContact));
 
-router.delete('/:contactId', ctrlWrapper(ctrl.removeContact));
+router.delete('/:contactId', isValidId, ctrlWrapper(ctrl.removeContact));
 
-router.put('/:contactId', validateBody(schemas.addSchema, "missing fields"), ctrlWrapper(ctrl.updateContact));
+router.put('/:contactId', isValidId, validateBody(schemas.addSchema, "missing fields"), ctrlWrapper(ctrl.updateContact));
+
+router.patch('/:contactId/favorite', isValidId, validateBody(schemas.updateFavoriteSchema, "missing field favorite"), ctrlWrapper(ctrl.updateStatusContact));
 
 module.exports = router;
